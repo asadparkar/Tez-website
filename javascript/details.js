75cbb999de0463e1e56603615b2c18ec70e7eb73
asad parkar <asadparkar69@gmail.com>
2022-09-27 00:54:23 +0530

    Added the third page

---
+++ javascript/details.js
@@ -0,0 +1,31 @@
+var zomato = document.querySelector('.zomato');
+var swiggy = document.querySelector('.swiggy');
+
+var ref = 0;
+
+zomato.addEventListener('click',()=>{
+    if (swiggy.classList.contains('highlight')){
+        swiggy.classList.remove('highlight');
+        zomato.classList.add('highlight');
+        zomato.style.opacity = "1";
+        swiggy.style.opacity = "0.5";
+    } else{
+        zomato.classList.add('highlight');
+        zomato.style.opacity = "1";
+        swiggy.style.opacity = "0.5";
+    }
+})
+
+swiggy.addEventListener('click',()=>{
+    if (zomato.classList.contains('highlight')){
+        zomato.classList.remove('highlight');
+        swiggy.classList.add('highlight');
+        zomato.style.opacity = "0.5";
+        swiggy.style.opacity = "1";
+
+    } else{
+        swiggy.classList.add('highlight');
+        zomato.style.opacity = "0.5";
+        swiggy.style.opacity = "1";
+    }
+})
